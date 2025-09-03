daadad6e68bc32e79aa4261cff374a8af865dc7b
Your Name <you@example.com>
2025-09-03 11:23:06 -0500

    .

--- solucion_examen.py
+++ solucion_examen.py
@@ -5,11 +5,19 @@ class Libro:
         self.fecha = fecha
         self.genero = genero
 
+def buscar_libro(titulo, lista):
+    existe = False
+    for libro in lista:
+        if libro.titulo == titulo:
+            existe = True
+    return existe
+
 
 lista_libros = []
 while True:
     print("Seleccione la opciòn deseada")
     print("1. Registrar libros")
+    print("2. Mostrar libros")
 
     opcion = int(input())
     if opcion == 1:
@@ -21,9 +29,17 @@ while True:
         lista_libros.append(libro_nuevo)
         print("Libro agregado exitosamente")
     if opcion == 2:
-        # for libro in lista_libros:
-        #     print(libro.autor)
-        #     print(libro.titulo)
-        print(lista_libros)
+        for libro in lista_libros:
+            print(libro.autor)
+            print(libro.titulo)
+    if opcion == 3:
+        titulo = input("Ingrese el nombre del libro")
+        encontrado = buscar_libro(titulo, lista_libros)
+        if encontrado == True:
+            print("Lo encontré")
+        else:
+            print("No lo encontré")
+    if opcion == 4:
+        print("La cantidad de libros es", len(lista_libros))
     elif opcion == 0:
         break
