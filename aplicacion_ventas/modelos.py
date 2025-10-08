84d891df07b34d9cb1d187ac20d234828c243ada
juan-gomez-stellion <81586639+juan-gomez-stellion@users.noreply.github.com>
2025-10-08 11:31:33 -0500

    Update modelos.py

--- aplicacion_ventas/modelos.py
+++ aplicacion_ventas/modelos.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, ClassVar, Set
+from excepciones import ProductosError, ClientesError
 
 @dataclass
 class Producto:
@@ -8,11 +9,34 @@ class Producto:
     categoria: str
     precio: float 
 
+    CATEGORIAS_PERMITIDAS: ClassVar[Set[str]] = {"alimentos", "servicios", "tecnología"}
+
+    def __post_init__(self) -> None:
+        try:
+            prueba = float(self.precio)
+        except:
+            raise ProductosError("Error en el precio del producto")
+        if prueba < 0:
+            raise ProductosError("El precio del producto no puede ser negativo")
+
+        self.categoria = self.categoria.lower()
+
+        if self.categoria not in self.CATEGORIAS_PERMITIDAS:
+            raise ProductosError(
+                f"Categoría no permitida: {self.categoria}. "
+                f"Categorías permitidas: {self.CATEGORIAS_PERMITIDAS}")
+
 @dataclass
 class Cliente:
     id: int
     nombre: str
     vip: bool
+
+    def __post_init__(self) -> None:
+        try:
+            prueba = int(self.id)
+        except:
+            raise ClientesError("Error en la identificación del cliente. Debe ser un número entero")
 
 @dataclass
 class LineaFactura:
@@ -21,4 +45,5 @@ class LineaFactura:
 
     @property
     def subtotal(self) -> float:
+
         return self.producto.precio * self.cantidad
